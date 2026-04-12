e2154b4377c51ec2e49147e1fae45557f5c75dd1
Alexandre COSTE <alexandre.coste42@gmail.com>
2026-04-12 22:04:52 +0200

    Feature: add parser module

---
+++ scraper/parser.py
@@ -0,0 +1,21 @@
+"""This module parse the response provide by the fecther to return the title,
+the price, the availability and the rating of the books in the current scrape
+page"""
+
+class Parser:
+    """
+    Parser class
+    """
+    def __init__(self, response: str):
+        """
+        Constructor initialisation
+        
+        :param response: Response of the requests provides by fetcher
+        :type response: str
+        """
+        self.response = response
+
+    def parse_title(self) -> str:
+        """
+        Parse the title of the book
+        """
