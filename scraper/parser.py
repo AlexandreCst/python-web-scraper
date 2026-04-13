3fe3b254332c300e849b210e3cb93d0a992e71ed
AlexandreCst <coste.alexandre09@gmail.com>
2026-04-13 22:22:24 +0200

    Feature: add _get_article private method and make parse_title method to extract the title of books of the current page

--- scraper/parser.py
+++ scraper/parser.py
@@ -2,6 +2,9 @@
 the price, the availability and the rating of the books in the current scrape
 page"""
 
+from bs4 import BeautifulSoup
+import requests
+
 class Parser:
     """
     Parser class
@@ -15,9 +18,20 @@ class Parser:
         """
         self.response = response
 
-    def parse_title(self) -> str:
+    def parse_title(self) -> list[str]:
         """
         Parse the title of the book
         """
-        
+        articles = self._get_article()
+        titles = [article.find("h3").a["title"] for article in articles]
+        return titles
 
+    def _get_article(self) -> list[str]:
+        """
+        Private method to build the soup object and get the article
+        
+        :param self: Description
+        """
+        soup = BeautifulSoup(self.response, "html.parser")
+        articles = soup.find_all("article", class_="product_pod")
+        return articles
