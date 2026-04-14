8a89421571d50d8ca0c1ac86ce8bde03ef4f6c35
AlexandreCst <coste.alexandre09@gmail.com>
2026-04-14 18:25:37 +0200

    Features: add public methods for parsing and orchestrate the parsing and private methods/functions to make parsing straightforward

--- scraper/parser.py
+++ scraper/parser.py
@@ -2,7 +2,7 @@
 the price, the availability and the rating of the books in the current scrape
 page"""
 
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup, Tag
 import requests
 
 class Parser:
@@ -17,15 +17,69 @@ class Parser:
         :type response: str
         """
         self.response = response
+        self.articles = self._get_article()
 
+    # Public methods - Parsing:
     def parse_title(self) -> list[str]:
         """
-        Parse the title of the book
+        Parse the title of the current page books and return the list of it
+        
+        :return: List of titles
+        :rtype: list[str]
         """
-        articles = self._get_article()
-        titles = [article.find("h3").a["title"] for article in articles]
+        titles = [article.find("h3").a["title"] for article in self.articles]
         return titles
 
+    def parse_price(self) -> list[float]:
+        """
+        Parse the price of the current page books and return the list of it
+        
+        :return: List of prices
+        :rtype: list[float]
+        """
+        prices = [float(self._extract_price(article)) for article in self.articles]
+        return prices
+    
+    def parse_availability(self) -> list[str]:
+        """
+        Parse the availability of the current page books and return the list of 
+        it
+        
+        :return: List of availabilities
+        :rtype: list[str]
+        """
+        availabilities = [self._extract_availability(article) for article in self.articles]
+        return availabilities
+    
+    def parse_rating(self) -> list[str]:
+        """
+        Parse the rating of the current page books and return the list of 
+        it
+        
+        :return: List of ratings
+        :rtype: list[str]
+        """
+        ratings = [self._extract_rating(article) for article in self.articles]
+        return ratings
+    
+    # Public method - Parsing orchestration:
+    def get_products(self) -> list[dict[str, str | float]]:
+        """
+        Method to orchestrate the whole parsing
+        
+        :return: List of dict with title, price, availability and rate for each book
+        :rtype: list[dict[str, str | float]]
+        """
+        title = self.parse_title()
+        price = self.parse_price()
+        availability = self.parse_availability()
+        rate = self.parse_rating()
+
+        products = [self._build_product(product) for product in zip(title, price, availability, rate)]
+        return products
+
+
+    # Private methods/functions
     def _get_article(self) -> list[str]:
         """
         Private method to build the soup object and get the article
@@ -35,3 +89,33 @@ class Parser:
         soup = BeautifulSoup(self.response, "html.parser")
         articles = soup.find_all("article", class_="product_pod")
         return articles
+    
+    def _extract_price(self, article: Tag) -> str:
+        """
+        Private function to format the price of the article
+        """
+        return article.find("div", class_="product_price").p.get_text(strip=True).replace("£", "")
+    
+    def _extract_availability(self, article: Tag) -> str:
+        """
+        Private function to format the availability of the article
+        """
+        return article.find("p", class_="instock availability").get_text(strip=True)
+    
+    def _extract_rating(self, article: Tag) -> str:
+        """
+        Private function to format the rating of the article 
+        """
+        return article.find("p", class_="star-rating").get_attribute_list("class")[1]
+    
+    def _build_product(self, product: tuple) -> dict[str, str | float]:
+        """
+        Private function to format the dict with title, price, availability and 
+        rating of a book
+        """
+        return {
+            "title": product[0],
+            "price": product[1],
+            "availability": product[2],
+            "rating": product[3]
+        }
